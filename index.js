d6b25a164bd3ccfa8fb3b72c1ed38f3ceefe7517
Read Langworthy <rlangworthy@gmail.com>
2021-11-01 15:04:44 -0700

    commit tree 20e627fa2061f1e8a26629f1aae32dae61382fbe

--- index.js
+++ index.js
@@ -21,3 +21,80 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(array) {
+    const employeeObj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeObj;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return ( arrayOfArrays.map(x => createEmployeeRecord(x) ) )
+} 
+
+let twoRows = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+]
+
+console.log(
+createEmployeeRecords(twoRows)
+)
+
+function createTimeInEvent(stringDate) {
+    const newObj = {
+        type: "TimeIn",
+        hour: parseInt( stringDate.slice(10, 15) ),
+        date: stringDate.slice(0, 10)
+    }
+    this.timeInEvents.push(newObj);
+    return this;
+}
+
+function createTimeOutEvent(stringDate) {
+    const newObj = {
+        type: "TimeOut",
+        hour: parseInt( stringDate.slice(10, 15) ),
+        date: stringDate.slice(0, 10)
+    }
+    this.timeOutEvents.push(newObj);
+    return this;
+}
+
+function hoursWorkedOnDate(stringDate) {
+    let index;
+    for (let i = 0; i < this.timeInEvents.length; i++) {
+        if ( stringDate === this.timeInEvents[i].date ) {
+            index = i;
+        }    
+    }
+
+    return( (this.timeOutEvents[index].hour - this.timeInEvents[index].hour ) / 100)
+}
+
+function wagesEarnedOnDate(stringDate) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, stringDate)
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    for (const i of srcArray) {
+        if (i.firstName === firstName) {
+            return i;
+        }
+    }
+    return undefined;
+}
+
+
+function calculatePayroll(arrayOfObjs) {
+    let sum = 0;
+    for (const i of arrayOfObjs) {
+        sum += allWagesFor.call(i);
+    }
+    return sum;
+}
